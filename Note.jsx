edc8880b2ec107a7dd68ac26dc52a4f03427c430
Ayanshsg19 <71961800+Ayanshsg19@users.noreply.github.com>
2021-06-06 18:33:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+     return (
+          <div className="note">
+                 <h1>JavaScript & React.js Bootcamp</h1>
+                 <p>
+                      This bootcamp was organised by <strong> SHAPE AI </strong> under the guidance of <strong> MR. SHAURYA SINHA SIR </strong>.
+                 </p>
+                 <p>
+                      In such a short duration of time he managed to teach the all the topics in a very elegant manner. He cleared all our doubts very well and never lost his cool, the claases were very interactive and informative. 
+                      <b />
+                      At last,  I would say i am delighted to be part a part of this bootcamp, and looking further to be a part in more. Thank You.
+                 </p>
+          </div>
+     );
+     
+export default Note;
